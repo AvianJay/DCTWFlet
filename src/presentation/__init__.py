2051f05579395bd21544a035d5e801bba3d34e6e
ParrotXray <b05817691@gmail.com>
2025-11-06 15:07:25 +0800

    refactor: Refactoring the system using DDD architecture

---
+++ src/presentation/__init__.py
@@ -0,0 +1,10 @@
+from .router import Router
+from .pages import BotListPage, ServerListPage, TemplateListPage, SettingsPage
+
+__all__ = [
+    "Router",
+    "BotListPage",
+    "ServerListPage",
+    "TemplateListPage",
+    "SettingsPage",
+]
---
+++ src/presentation/pages/__init__.py
@@ -0,0 +1,13 @@
+"""Presentation pages"""
+
+from .bot_list_page import BotListPage
+from .server_list_page import ServerListPage
+from .template_list_page import TemplateListPage
+from .settings_page import SettingsPage
+
+__all__ = [
+    "BotListPage",
+    "ServerListPage",
+    "TemplateListPage",
+    "SettingsPage",
+]
